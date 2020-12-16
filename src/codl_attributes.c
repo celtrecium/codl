25419a79ecabd055fb91860f24d0d1c2d7e88155
celt <digebin@gmail.com>
2020-12-16 18:56:00 +0500

    Update

--- src/codl_attributes.c
+++ src/codl_attributes.c
@@ -21,3 +21,25 @@ int codl_remove_attribute(codl_window *win, char attribute) {
 
     return(1);
 }
+
+
+int codl_set_attribute(codl_window *win, char attribute) {
+    char t_attr_tmp;
+
+    CODL_NULLPTR_MACRO(!win, "Window pointer for set attribute is NULL")
+    CODL_NULLPTR_MACRO(!win->window_buffer, "Window buffer for set attribute is NULL")
+
+    t_attr_tmp = win->text_attribute;
+    win->text_attribute = attribute;
+
+
+    if((t_attr_tmp & DEF_COLOUR_BG_BIT) == DEF_COLOUR_BG_BIT) {
+        win->text_attribute |= DEF_COLOUR_BG_BIT;
+    }
+
+    if((t_attr_tmp & DEF_COLOUR_FG_BIT) == DEF_COLOUR_FG_BIT) {
+        win->text_attribute |= DEF_COLOUR_FG_BIT;
+    }
+
+    return(1);
+}
