df3f6635c94b4119e4b75b78cdaa949d1cce4320
Gebet94 <alexeysonne@gmail.com>
2021-03-28 10:41:05 +0200

    Add codl_get_key() implementation for Windows

--- src/codl_input.c
+++ src/codl_input.c
@@ -1,14 +1,35 @@
 #include "codl_internal.h"
 
+static int __read_char_from_stdin(void) {
+#if defined(__unix__)
+    return getchar();
+#elif defined(_WIN32)
+    int key = 0;
+    DWORD bytes_read = 0;
+    DWORD unread_records = 0;
+    BOOL success = FALSE;
+    HANDLE stdin_handle = GetStdHandle(STD_INPUT_HANDLE);
+
+    GetNumberOfConsoleInputEvents(stdin_handle, &unread_records);
+    unread_records -= 1;
+
+    if (!unread_records) {
+        return EOF;
+    }
+
+    success = ReadConsole(stdin_handle, (LPDWORD) &key, 1, &bytes_read, NULL);
+
+    return success && bytes_read ? key : EOF;
+#endif /* __unix__ */
+}
 
 unsigned int codl_get_key(void) {
 #if defined(__unix__)
     int  oldf;
     struct termios newt, oldt;
-#elif defined(_WIN32) || defined(__CYGWIN__)
-    DWORD old_mode = 0;
+#elif defined(_WIN32)
+    HANDLE stdin_handle = GetStdHandle(STD_INPUT_HANDLE);
     DWORD mode = 0;
-    char key[10];
 #endif
     int tmp              = 0;
     int count            = 0;
@@ -21,25 +42,22 @@ unsigned int codl_get_key(void) {
     tcsetattr(0, TCSANOW, &newt);
     oldf = fcntl(0, F_SETFL, 0);
     fcntl(0, F_SETFL, oldf | O_NONBLOCK);
-#elif defined(_WIN32) || defined(__CYGWIN__)
-  GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &old_mode);
-  mode = (DWORD)(~ENABLE_LINE_INPUT);
+#elif defined(_WIN32)
+    GetConsoleMode(stdin_handle, &mode);
 
-  SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), mode);
-  setvbuf(stdin, NULL, _IONBF, 0);
-  codl_memset(key, 10, 0, 10);
+    SetConsoleMode(stdin_handle, mode & (unsigned long) ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
 #endif
 
-#if defined(__unix__)
-    tmp = getchar();
+    tmp = __read_char_from_stdin();
 
-    if(tmp == EOF) {
+    if (tmp == EOF) {
+#if defined(__unix__)
         tcsetattr(0, TCSANOW, &oldt);
         fcntl(0, F_SETFL, oldf);
-#elif defined(_WIN32) || defined(__CYGWIN__)
-    if(!kbhit()) {
-        SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), old_mode);
+#elif defined(_WIN32)
+        SetConsoleMode(stdin_handle, mode);
 #endif
+
         return(0);
     } else if((UTF8_CODEPOINT_4B & tmp) == UTF8_CODEPOINT_4B || 
               (UTF8_CODEPOINT_3B & tmp) == UTF8_CODEPOINT_3B ||
@@ -48,28 +66,22 @@ unsigned int codl_get_key(void) {
 
         for(; tmp != EOF && count < UNICODE_CHAR_SIZE; ++count) {
             unicode_char[count] = (char)tmp;
-            tmp                 = fgetc(stdin);
+            tmp                 = __read_char_from_stdin();
         }
 
         return(CODL_KEY_UNICODE);
     }
 
-#if defined(__unix__)
     while(tmp != EOF) {
         tmp_key = (tmp_key * (tmp > 100 ? 1000 : 100)) + (unsigned int)tmp;
-        tmp     = getchar();
+        tmp     = __read_char_from_stdin();
     }
 
+#if defined(__unix__)
     tcsetattr(0, TCSANOW, &oldt);
     fcntl(0, F_SETFL, oldf);
-#elif defined(_WIN32) || defined(__CYGWIN__)
-
-    for(count = 0; key[count];) {
-        tmp_key = (tmp_key * (key[count] > 100 ? 1000 : 100)) + (unsigned int)key[count];
-        *key = getch();
-    }
-
-    SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), old_mode);
+#elif defined(_WIN32)
+    SetConsoleMode(stdin_handle, mode);
 #endif
 
     return(tmp_key);
